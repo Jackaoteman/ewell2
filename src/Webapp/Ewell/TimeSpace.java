7332e003ade31e57fb90e269f1912bc7234ab600
Jack <Jack@163.com>
2019-08-24 15:45:54 +0800

    再次修改

--- src/Webapp/Ewell/TimeSpace.java
+++ src/Webapp/Ewell/TimeSpace.java
@@ -9,14 +9,14 @@ import java.util.Date;
  */
 public class TimeSpace {
     public static void main(String[] args) {
-        System.out.println("方方方方方方泛");
-        System.out.println("回回回回回回回hi回回回回u会");
-        System.out.println("娟娟1111111111111111111111111");;
         System.out.println("啊······················   啊啊啊啊啊啊啊啊啊啊啊 ");
         System.out.println("啊······················   啊啊啊啊啊啊啊啊啊啊啊 ");
         System.out.println("啊······················   啊啊啊啊啊啊啊啊啊啊啊 ");
         Date  date=new Date();
         System.out.println(date);
+
+
+
         System.out.println("我不知道。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。");
         SimpleDateFormat  simpleDateFormat=new SimpleDateFormat("HH:mm");
         String format = simpleDateFormat.format(date);
